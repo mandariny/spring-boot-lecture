fbb26b9642c276fc2315a2b1d7a32fb0267c13a3
devshy <dev.sohee.y@gmail.com>
2024-09-27 15:00:12 +0900

    [24.09.27] Spring Data Jpa 페이징, 정렬

--- spring-data-jpa-practice/src/main/java/study/spring_data_jpa_practice/controller/MemberController.java
+++ spring-data-jpa-practice/src/main/java/study/spring_data_jpa_practice/controller/MemberController.java
@@ -2,9 +2,13 @@ package study.spring_data_jpa_practice.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.spring_data_jpa_practice.dto.MemberDto;
 import study.spring_data_jpa_practice.entity.Member;
 import study.spring_data_jpa_practice.repository.MemberRepository;
 
@@ -32,8 +36,37 @@ public class MemberController {
         return member.getUsername();
     }
 
+    /*
+    *
+    * localhost:8080/members?page=0&size=3&sort=id,desc
+    * page, size, sort 조건을 주면 Controller에서 Parameter 값을 PageRequest 객체로 생성해서 Pageable에 injection 해줌
+    * application.yml에 default page size, max page size 등을 global로 설정할 수 있음
+    *
+    * @PageableDefault로 특정 메소드에만 설정을 줄 수도 있음
+    * global 설정보다 우선순위가 높음
+    *
+    * 페이징 정보가 둘 이상일 경우 접두사로 구분할 수 있음
+    * @Qualifier("member") Pageable memberPageable, @Qualifier("order") Pageable orderPageable ...
+    * localhost:8080/members?member_page=0&order_page=1
+    *
+    */
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        // 실무에선 Entity를 그대로 노출하면 안 됨,,,
+        return memberRepository.findAll(pageable);
+    }
+
+    @GetMapping("/members2")
+    public Page<MemberDto> list2(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+//        return memberRepository.findAll(pageable)
+//                .map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("memberA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
--- spring-data-jpa-practice/src/main/java/study/spring_data_jpa_practice/dto/MemberDto.java
+++ spring-data-jpa-practice/src/main/java/study/spring_data_jpa_practice/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.spring_data_jpa_practice.dto;
 
 import lombok.Data;
+import study.spring_data_jpa_practice.entity.Member;
 
 // 단순 DTO가 아닌 엔티티에는 @Data 사용하지 않기
 @Data
@@ -13,5 +14,13 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    // Entity는 DTO를 가급적 보지 않는 것이 좋음
+    // DTO는 Entity를 봐도 상관 없음
+    // Entity는 Application 내부에서 공용으로 사용되는 것
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
